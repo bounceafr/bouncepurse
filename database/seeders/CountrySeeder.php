ff5b0b678a97b81f5a201834bda9607e3e3d58bb
Jacques Mbabazi <mbabazijacques@gmail.com>
2026-06-17 11:43:33 +0200

    wip: work in progress

--- database/seeders/CountrySeeder.php
+++ database/seeders/CountrySeeder.php
@@ -4,16 +4,13 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\Country;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Artisan;
 
 final class CountrySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        Artisan::call('app:import-countries-command');
+        Country::factory()->count(50)->create();
     }
 }
