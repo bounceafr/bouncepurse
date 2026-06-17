56e4ab66c67ea3ba3d10b20b2dcab3b68e950d64
Jacques Mbabazi <mbabazijacques@gmail.com>
2026-06-17 13:11:55 +0200

    wip: work in progress

--- resources/js/components/ui/calendar.tsx
+++ resources/js/components/ui/calendar.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon, ChevronRightIcon, ChevronDownIcon } from "lucide-react"
 import * as React from "react"
 import {
   DayPicker,
@@ -6,9 +7,8 @@ import {
   type Locale,
 } from "react-day-picker"
 
-import { cn } from "@/lib/utils"
 import { Button, buttonVariants } from "@/components/ui/button"
-import { ChevronLeftIcon, ChevronRightIcon, ChevronDownIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
 
 function Calendar({
   className,
